72f5c563067bb8cb5ebfd3ce0be69d36241e1cf7
Rafal Rajtar <rajtar.rafal@gmail.com>
2019-02-09 13:19:54 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./lib/airr-react.css";
+import "airr-react/dist/airr-react.css";
 import "./app.css";
 import App from "./App";
 
 //when using Cordova for mobile apps, wait for device to be ready and then render
 if (window.cordova) {
-    document.addEventListener("deviceready", function() {
-        ReactDOM.render(<App />, document.getElementById("root"));
-    });
-} else {
+  document.addEventListener("deviceready", function() {
     ReactDOM.render(<App />, document.getElementById("root"));
+  });
+} else {
+  ReactDOM.render(<App />, document.getElementById("root"));
 }
