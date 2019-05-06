b0d745eae78d7c117380bb9a898a45ea591e6dfa
Rafal Rajtar <rafal.rajtar@consultant.volvo.com>
2019-05-06 22:10:34 +0200

    cordova entry removal

--- src/index.js
+++ src/index.js
@@ -4,11 +4,4 @@ import "airr-react/dist/airr-react.css";
 import "./app.css";
 import App from "./App";
 
-//when using Cordova for mobile apps, wait for device to be ready and then render
-if (window.cordova) {
-  document.addEventListener("deviceready", function() {
-    ReactDOM.render(<App />, document.getElementById("root"));
-  });
-} else {
-  ReactDOM.render(<App />, document.getElementById("root"));
-}
+ReactDOM.render(<App />, document.getElementById("root"));
